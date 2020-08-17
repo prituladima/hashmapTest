3906b50a054115f8a3841b93abfc7ac47eb57962
prituladima <prituladima@gmail.com>
2020-08-17 19:38:20 +0200

    Very good results 3

--- run.sh
+++ run.sh
@@ -3,3 +3,6 @@
 #jarsigner -verify -verbose -certs target/benchmarks.jar
 #jarsigner -verify target/benchmarks.jar
 java -cp target/benchmarks.jar tests.MapTestRunner
+
+
+mvn exec:java  -Dexec.mainClass=ManualTests
